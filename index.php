54b358e7536649bda6714d35784b6c7b11ad7d0a
Kan090808 <s104213059@mail1.ncnu.edu.tw>
2019-10-13 16:23:35 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php require("header.php");?>
+
+<div class="row">
+    <div class="col-md-1">
+        <a href="search.php" class="btn btn-info" role="button">Search</a>
+    </div>
+    <div class="col-md-1">
+        <a href="setting.php" class="btn btn-warning" role="button">Setting</a>
+    </div>
+</div>
---
+++ search.php
@@ -0,0 +1,8 @@
+<?php require("header.php");?>
+
+<div class="row">
+    <h2>Search</h2>
+</div>
+<div class="row">
+    
+</div>
